440dfbd4988fb5260a63c6ee0db672316dc90b3d
Karmt45 <96269362+Karmt45@users.noreply.github.com>
2022-12-31 15:29:18 -0800

    betty style code task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,25 +1,18 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <ctype.h>
 /**
- * main - Entry point
- *
- * Description: print 0-9 using putchar
- *              while using int variable
- *
- * Return: Always 0 (Success)
+*main - Prints all single digits of base 10 followed by new line
+*
+*Return: returns 0
 */
-
 int main(void)
 {
-	int digit = 0;
+	int digit;
 
-	while (digit <= 9)
+	for (digit = '0'; digit <= '9'; digit++)
 	{
-		/*convert digit to ASCII representation*/
-		putchar(digit + '0');
-		++digit;
+		putchar(digit);
 	}
 	putchar('\n');
-
 	return (0);
 }
